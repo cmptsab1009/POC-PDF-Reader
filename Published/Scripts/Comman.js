ab4efad688d780cec82a9f85bc6af819a72e7315
Narendra <narendramehra@cdnsol.com>
2019-07-12 20:26:27 +0530

    First commit

---
+++ Published/Scripts/Comman.js
@@ -0,0 +1,8 @@
+﻿var globalapp = {};
+var getUrl = window.location.origin;
+var getPathName = window.location.pathname.split('/')[1];
+if (getPathName != "" && getPathName != "Home") {
+    globalapp.baseUrl = getUrl + "/" + getPathName;
+} else {
+    globalapp.baseUrl = getUrl;
+}
